ec6be397700069c591515b843a82d7563e609d50
Bui Quang Tri <bqt1816@gmail.com>
2020-10-15 15:43:45 +0700

    add base view and model

---
+++ Pokedex/View/PokemonView/Detail/MovesView.swift
@@ -0,0 +1,20 @@
+//
+//  MovesView.swift
+//  Pokedex
+//
+//  Created by Bui Quang Tri on 10/15/20.
+//
+
+import SwiftUI
+
+struct MovesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovesView()
+    }
+}
