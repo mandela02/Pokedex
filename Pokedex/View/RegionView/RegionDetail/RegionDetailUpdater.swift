1d7ee34f252aacb05678382ce1a1fd34e53d1774
TriBQ <10khongquang@gmail.com>
2020-11-20 21:22:33 +0700

    change picker type

--- Pokedex/View/RegionView/RegionDetail/RegionDetailUpdater.swift
+++ Pokedex/View/RegionView/RegionDetail/RegionDetailUpdater.swift
@@ -15,8 +15,39 @@ class RegionDetailUpdater: ObservableObject {
             getRegion(from: url)
         }
     }
-    @Published var region: Region?
-    @Published var selectedLocation: String = ""
+    
+    @Published var region: Region? {
+        didSet {
+            guard let region = region else { return }
+            locations = [selectedLocation] + region.locations.map({$0.name.capitalizingFirstLetter().eliminateDash})
+            getPokedex(from: region)
+        }
+    }
+    
+    @Published var locations: [String] = []
+    @Published var selectedLocation: String = "default" {
+        didSet {
+            if firstTimeLoading && selectedLocation != "default" {
+                legionName = selectedLocation
+                firstTimeLoading = false
+            }
+            
+            if selectedLocation == legionName {
+                getPokedex(from: region)
+            } else {
+                pokedexNames.removeAll()
+            }
+        }
+    }
+    private var legionName = ""
+    private var firstTimeLoading = true
+    @Published var pokedexNames: [String] = [] {
+        didSet {
+            isHavingMultiDex = pokedexNames.count > 1
+        }
+    }
+    @Published var isHavingMultiDex = false
+    @Published var selectedPokedex: String = ""
 
     func getRegion(from url: String?) {
         guard let url = url else { return }
@@ -26,5 +57,13 @@ class RegionDetailUpdater: ObservableObject {
             .sink { region in
                 self.region = region
             }.store(in: &cancellables)
+    }
+    
+    func getPokedex(from region: Region?) {
+        guard let region = region else { return }
+        let dexs = region.pokedexes
+        if dexs.isEmpty { return }
+        pokedexNames = dexs.map({$0.name.eliminateDash.capitalizingFirstLetter()})
+        selectedPokedex = pokedexNames.first ?? ""
     }
 }
--- Pokedex/View/RegionView/RegionDetail/RegionDetailView.swift
+++ Pokedex/View/RegionView/RegionDetail/RegionDetailView.swift
@@ -36,6 +36,7 @@ struct RegionDetailView: View {
         }
         .onAppear {
             updater.url = regionModel.url
+            updater.selectedLocation = regionModel.name.capitalizingFirstLetter()
         }
         .navigationTitle("")
         .navigationBarHidden(true)
@@ -45,16 +46,58 @@ struct RegionDetailView: View {
 
 struct LocationPickerView: View {
     @ObservedObject var updater: RegionDetailUpdater
+ 
+    var selectedLocationLabel: some View {
+        Text(updater.selectedLocation)
+            .font(Biotif.bold(size: 20).font)
+            .foregroundColor(.blue)
+    }
     
-    var body: some View {
-        Form {
-            Picker(selection: $updater.selectedLocation, label: Text("Location")) {
-                ForEach(updater.region?.locations.map({$0.name.capitalizingFirstLetter().eliminateDash}) ?? []) {
-                    Text($0)
+    var selectedPokedexLabel: some View {
+        Text(updater.selectedPokedex)
+            .font(Biotif.bold(size: 20).font)
+            .foregroundColor(.blue)
+    }
 
-                }
+    var body: some View {
+        VStack(spacing: 10) {
+            HStack {
+                Text("Location")
+                    .font(Biotif.regular(size: 20).font)
+                    .foregroundColor(.black)
+                Spacer()
+                Picker(selection: $updater.selectedLocation,
+                       label: selectedLocationLabel) {
+                    ForEach(updater.locations) {
+                        Text($0)
+                    }
+                }.pickerStyle(MenuPickerStyle())
+            }.frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 2)
+            
+            if updater.isHavingMultiDex {
+                HStack {
+                    Text("Pokedex")
+                        .font(Biotif.regular(size: 20).font)
+                        .foregroundColor(.black)
+                    Spacer()
+                    Picker(selection: $updater.selectedPokedex,
+                           label: selectedPokedexLabel) {
+                        ForEach(updater.pokedexNames) {
+                            Text($0)
+                        }
+                    }.pickerStyle(MenuPickerStyle())
+                }.frame(minWidth: 0, maxWidth: .infinity)
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(radius: 2)
             }
-        }.frame(minWidth: 0, maxWidth: .infinity)
-        .frame(height: 100, alignment: .center)
+        }
+        .transition(.opacity)
+        .animation(Animation.easeIn(duration: 0.2))
     }
 }
