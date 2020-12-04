23e46b4414d719a43b9dc0d4cb8beffe7685341b
Bui Quang Tri <bqt1816@gmail.com>
2020-12-04 09:51:19 +0700

    no message

--- Pokedex/PokedexApp.swift
+++ Pokedex/PokedexApp.swift
@@ -19,3 +19,51 @@ struct PokedexApp: App {
         }
     }
 }
+
+
+struct ContentViewExample: View {
+    @State private var offset = CGSize.zero
+    @State private var isDragging = false
+
+    var body: some View {
+        let dragGesture = DragGesture()
+            .onChanged { value in self.offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    self.offset = .zero
+                    self.isDragging = false
+                }
+            }
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    self.isDragging = true
+                }
+            }
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        return Circle()
+            .fill(Color.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+    }
+}
+
+struct GestureExample: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .onTapGesture {
+                    print("Text tapped")
+                }
+        }
+        .highPriorityGesture(
+            TapGesture()
+                .onEnded { _ in
+                    print("VStack tapped")
+                }
+        )
+    }
+}
